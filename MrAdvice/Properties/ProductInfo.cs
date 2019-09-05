e7f895cfff8f75e0ca427cc5017fb0b75e5cdd01
Alexander Buts <Alexb@intern.blackwood.gg>
2019-09-05 15:39:22 +0300

    MrAdvice works with dotnet core. Tests don't work.

--- MrAdvice/Properties/ProductInfo.cs
+++ MrAdvice/Properties/ProductInfo.cs
@@ -1,5 +1,4 @@
-﻿
-#region Mr. Advice
+﻿#region Mr. Advice
 // Mr. Advice
 // A simple post build weaving package
 // http://mradvice.arxone.com/
@@ -7,30 +6,22 @@
 #endregion
 
 using System.Reflection;
-using System.Resources;
+
 using ArxOne.MrAdvice.Properties;
 
-[assembly: AssemblyDescription("MrAdvice allows to weave aspects at build-time (just like PostSharp, but free as in free beer). Write your own aspects in the form of attributes and apply them to target methods or properties. This version does not require any dependency (on the opposite of MrAdvice.Fody which requires Fody).")]
-[assembly: AssemblyCompany("Arx One")]
-[assembly: AssemblyProduct("Mr. Advice")]
-[assembly: AssemblyCopyright("MIT license http://opensource.org/licenses/mit-license.php")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
-
 [assembly: AssemblyVersion("2")]
-[assembly: AssemblyFileVersion("2.8.5")] // 
+[assembly: AssemblyFileVersion(Product.Version)]
 
 namespace ArxOne.MrAdvice.Properties
 {
-    /// <summary>
-    /// Informations about product
-    /// </summary>
-    internal static class Product
-    {
-        /// <summary>
-        /// The version
-        /// </summary>
-        public const string Version = "2.8.5";
-    }
+	/// <summary>
+	/// Informations about product
+	/// </summary>
+	public static class Product
+	{
+		/// <summary>
+		/// The version
+		/// </summary>
+		public const string Version = "2.8.5";
+	}
 }
